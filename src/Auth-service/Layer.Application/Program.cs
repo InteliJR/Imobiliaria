b7240115ccb2b12afdd85bec8f71b32a3900d3c1
Matheus Ribeiro dos Santos <124204749+omatheu@users.noreply.github.com>
2024-09-16 20:51:29 -0300

    add: gitignore Rebase

--- src/Auth-service/Layer.Application/Program.cs
+++ src/Auth-service/Layer.Application/Program.cs
@@ -1,150 +1,15 @@
-﻿using Microsoft.OpenApi.Models;
-using Layer.Domain.Interfaces;
-using Layer.Services;
-using Layer.Services.Services;
-using Layer.Infrastructure.Database;
-using Microsoft.EntityFrameworkCore;
-using DotNetEnv;
-using Microsoft.Extensions.Options;
-using Microsoft.Extensions.Configuration;
-using Layer.Domain.Entities;
-using Layer.Domain.Enums;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-
-
 var builder = WebApplication.CreateBuilder(args);
-
-// Carregar variáveis de ambiente do arquivo .env
-Env.Load();
-
-// Sobrepor os valores das variáveis no appsettings.json com as variáveis do ambiente
-builder.Configuration.AddEnvironmentVariables();
-
-// !!!!! Injenções de dependência !!!!!
-
-// builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(
-        builder.Configuration.GetConnectionString("DefaultConnection"),
-        npgsqlOptions =>
-        {
-            npgsqlOptions.EnableRetryOnFailure(
-                maxRetryCount: 5,
-                maxRetryDelay: TimeSpan.FromSeconds(30),
-                errorCodesToAdd: null);
-            npgsqlOptions.CommandTimeout(30); // Timeout de 30 segundos
-        })
-    .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking) // Desabilitar rastreamento de mudanças para melhorar a performance
-);
-
-/*builder.Services.AddDbContext<AppDbContext>(o => o.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-*/
-// Vamos usar o AddSingleton usar a msm instância em usada em toda a aplicação
-// Se usamos o AddScoped, ele cria uma instância por requisição aí ele vai zerar a lista de mensagens a cada requisição
-builder.Services.AddScoped<IMessageService, MessageService>();
-
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<ITokenService, TokenService>();
-
-// Configura JWT settings
-var jwtSettings = new JwtSettings
-{
-    SecretKey = Environment.GetEnvironmentVariable("JwtSettings__SecretKey"),
-    ExpiryMinutes = int.Parse(Environment.GetEnvironmentVariable("JwtSettings__ExpiryMinutes")),
-    Issuer = Environment.GetEnvironmentVariable("JwtSettings__Issuer"),
-    Audience = Environment.GetEnvironmentVariable("JwtSettings__Audience")
-};
-builder.Services.AddSingleton(jwtSettings);
-
-// Configura autenticação JWT
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.SaveToken = true;
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = jwtSettings.Issuer,
-        ValidAudience = jwtSettings.Audience,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.SecretKey))
-    };
-});
-
-builder.Services.AddControllers();
-builder.Services.AddSwaggerGen();
-
-
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-// Configura o Swagger com o JWT
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Serviço de Gerenciamento de usuários API", Version = "v1" });
-
-    // Configura��o para exibir a op��o de autentica��o Bearer
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        In = ParameterLocation.Header,
-        Description = "Please enter JWT with Bearer into field",
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer"
-    });
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
-    {
-        new OpenApiSecurityScheme {
-            Reference = new OpenApiReference {
-                Type = ReferenceType.SecurityScheme,
-                Id = "Bearer"
-            }
-        },
-        new string[] { }
-    }});
-});
-
-// Configurar roles para o JWT
-
-builder.Services.AddAuthorization(options =>
-{
-    options.AddPolicy(nameof(Roles.Admin), policy => policy.RequireRole(nameof(Roles.Admin)));
-    options.AddPolicy(nameof(Roles.Locador), policy => policy.RequireRole(nameof(Roles.Locador)));
-    options.AddPolicy(nameof(Roles.Locatario), policy => policy.RequireRole(nameof(Roles.Locatario)));
-});
-
-builder.Logging.ClearProviders();
-builder.Logging.AddConsole();
-builder.Logging.AddDebug();
-
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Serviço de gestão de usuários");
-        c.RoutePrefix = string.Empty; // Para carregar o Swagger na raiz (http://localhost:<port>/)
-    });
-}else
+if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -153,7 +18,6 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
