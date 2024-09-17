6887aed01447ffc30cf3f27ce1fbcbdee22be66b
Rodrigo-07 <rodrigo.santos@sou.inteli.edu.br>
2024-09-17 01:08:20 -0300

    feat: exemplo fluxo

--- src/Auth-service/Layer.Application/Program.cs
+++ src/Auth-service/Layer.Application/Program.cs
@@ -1,15 +1,64 @@
+﻿using Microsoft.OpenApi.Models;
+using Layer.Domain.Interfaces;
+using Layer.Services;
+using Layer.Services.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// !!!!! Injenções de dependência !!!!!
+
+// Vamos usar o AddSingleton usar a msm instância em usada em toda a aplicação
+// Se usamos o AddScoped, ele cria uma instância por requisição aí ele vai zerar a lista de mensagens a cada requisição
+builder.Services.AddSingleton<IMessageService, MessageService>();
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Configura o Swagger com o JWT
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Serviço de Gerenciamento de usuários API", Version = "v1" });
+
+    // Configura��o para exibir a op��o de autentica��o Bearer
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter JWT with Bearer into field",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+    {
+        new OpenApiSecurityScheme {
+            Reference = new OpenApiReference {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+            }
+        },
+        new string[] { }
+    }});
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Serviço de gestão de usuários");
+        c.RoutePrefix = string.Empty; // Para carregar o Swagger na raiz (http://localhost:<port>/)
+    });
+}else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
---
+++ src/Auth-service/Layer.Domain/Entities/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Layer.Domain.Entities
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string MenssageContent {  get; set; }
+
+    }
+}
