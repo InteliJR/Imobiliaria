57da086652bafdb8bf59c585f57a3f59e3e8545e
Rodrigo-07 <rodrigo.santos@sou.inteli.edu.br>
2024-09-17 12:45:27 -0300

    feat: conectando no supabase

---
+++ src/Auth-service/Layer.Application/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Layer.Domain.Interfaces;
+
+namespace Layer.Application.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : Controller
+    {
+        // Chamar o serviço de usuário
+        private readonly IUserService _userService;
+
+        // Construtor
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        // Rota de pagar todos os usuários
+        [HttpGet("PegarTodosUsuarios")]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var users = await _userService.GetUsuariosAsync();
+            return Ok(users);
+        }
+    }
+}
---
+++ src/Auth-service/Layer.Domain/Interfaces/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Layer.Domain.Entities;
+
+namespace Layer.Domain.Interfaces
+{
+    public interface IUserService
+    {
+        Task<List<User>> GetUsuariosAsync();
+    }
+}
