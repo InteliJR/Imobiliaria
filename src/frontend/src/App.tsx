9eda99c61c3ed50249b356f8dbf0377996f4f426
olin-med <olin.costa@sou.inteli.edu.br>
2024-09-19 10:42:48 -0300

    feat: adds frotend files to the project (Vite React TS + Tailwind + working router(main.tsx)

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,10 +1,7 @@
-import Navbar from "./components/Navbar"
-
-
 export default function App() {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -5,12 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        'neutral-black': '#1F1E1C',
-        'yellow-middle': '#F2B705',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
