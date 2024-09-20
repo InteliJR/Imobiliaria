233446b78bcc533215b29591da60cb463737bb61
Matheus Ribeiro dos Santos <124204749+omatheu@users.noreply.github.com>
2024-09-20 10:48:12 -0300

    feat: structure and the first method of api

---
+++ src/Property-service/Layer.Services/Services/ImoveisService.cs
@@ -0,0 +1,27 @@
+﻿using Layer.Domain.Entities;
+using Layer.Domain.Interfaces;
+using Layer.Infrastructure;
+using Layer.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Layer.Services.Services
+{
+    public class ImoveisService : Iimoveis
+    {
+
+        // Vamo adicionar o contexto do banco de dados
+        private readonly ApplicationDbContext _dbcontext;
+
+        // Constructor
+        public ImoveisService(ApplicationDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+        public Task<List<Imoveis>> GetImoveisAsync()
+        {
+            return _dbcontext.Imoveis.ToListAsync(); // Retornando a lista de imoveis de maneira assincrona
+        }
+    }
+}
