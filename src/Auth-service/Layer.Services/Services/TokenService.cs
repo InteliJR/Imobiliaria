f6c47c9d0181ac349b1ee1f22bfb3aa267473c9c
Rodrigo-07 <rodrigo.santos@sou.inteli.edu.br>
2024-09-25 01:14:03 -0300

    Aplicando regras do .gitignore

---
+++ src/Auth-service/Layer.Services/Services/TokenService.cs
@@ -0,0 +1,51 @@
+﻿using Layer.Domain.Entities;
+using Layer.Domain.Enums;
+using Layer.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+
+namespace Layer.Services.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly JwtSettings _jwtSettings;
+
+        public TokenService(JwtSettings jwtSettings)
+        {
+            _jwtSettings = jwtSettings;
+        }
+
+        public string GenerateToken(User user)
+        {
+            var role = Roles.Admin.ToString(); // Pega o role do usuário !!!PUXAR DO BANCO DE DADOS!!!
+
+            var claims = new List<Claim>
+            {
+                // Lista de coisas/infos que o token vai ter
+                new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.Role, role) // Adiciona o role no token
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.SecretKey));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _jwtSettings.Issuer,
+                audience: _jwtSettings.Audience,
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(_jwtSettings.ExpiryMinutes),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
