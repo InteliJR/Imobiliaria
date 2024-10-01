8f8771aee1cdba3f6b7421f5781e9d2b23f968dd
Matheus Ribeiro dos Santos <124204749+omatheu@users.noreply.github.com>
2024-10-01 11:33:26 -0300

    feat: initial crud

---
+++ src/Property-service/Layer.Domain/Entites/Contratos.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Layer.Domain.Entities;
+
+namespace Layer.Domain.Entites
+{
+    [Table("contratos")]
+    public class Contratos
+    {
+        [Key]
+        [Column("contratoid")] // Nome da coluna no banco de dados
+        public int ContratoId { get; set; }
+
+        [Column("documentos", TypeName = "varchar")] // Nome da coluna e tipo no banco de dados
+        public string Documentos { get; set; }
+
+        [Column("valor_aluguel")] // Nome da coluna no banco de dados
+        public decimal ValorAluguel { get; set; }
+
+        [Column("data_inicio")] // Nome da coluna no banco de dados
+        public DateTime DataInicio { get; set; }
+
+        [Column("data_encerramento")] // Nome da coluna no banco de dados
+        public DateTime? DataEncerramento { get; set; }
+
+        [ForeignKey("Locador")]
+        [Column("locadorid")] // Nome da coluna no banco de dados
+        public int LocadorId { get; set; }
+
+        [ForeignKey("Locatario")]
+        [Column("locatarioid")] // Nome da coluna no banco de dados
+        public int LocatarioId { get; set; }
+
+        [ForeignKey("Imovel")]
+        [Column("imovelid")] // Nome da coluna no banco de dados
+        public int ImovelId { get; set; }
+
+        [Column("tipo_garantia", TypeName = "varchar")] // Nome da coluna e tipo no banco de dados
+        public string TipoGarantia { get; set; }
+
+        [Column("condicoes_especiais", TypeName = "text")] // Nome da coluna no banco de dados
+        public string CondicoesEspeciais { get; set; }
+
+        [Column("status", TypeName = "varchar")] // Nome da coluna e tipo no banco de dados
+        public string Status { get; set; }
+
+        [Column("iptu")] // Nome da coluna no banco de dados
+        public decimal? Iptu { get; set; }
+
+        [Column("data_pagamento")] // Nome da coluna no banco de dados
+        public DateTime? DataPagamento { get; set; }
+
+        [Column("taxa_adm")] // Nome da coluna no banco de dados
+        public decimal? TaxaAdm { get; set; }
+
+        [Column("data_rescisao")] // Nome da coluna no banco de dados
+        public DateTime? DataRescisao { get; set; }
+
+        [Column("renovado")] // Nome da coluna no banco de dados
+        public bool Renovado { get; set; }
+
+        [Column("data_encerramento_renovacao")] // Nome da coluna no banco de dados
+        public DateTime? DataEncerramentoRenovacao { get; set; }
+
+        [Column("valor_reajuste")] // Nome da coluna no banco de dados
+        public decimal? ValorReajuste { get; set; }
+
+        // Relacionamentos com outras entidades
+        //public virtual Imovel Imovel { get; set; }
+    }
+}
--- src/Property-service/Layer.Insfrastructure/Database/ApplicationDbContext.cs
+++ src/Property-service/Layer.Insfrastructure/Database/ApplicationDbContext.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Layer.Domain.Entities;
+using Layer.Domain.Entites;
 
 namespace Layer.Infrastructure.Database
 {
@@ -14,44 +15,6 @@ namespace Layer.Infrastructure.Database
 
         // Mapear a tabela de imoveis
         public DbSet<Imoveis> Imoveis { get; set; }
-
-        // Configurações adicionais no método OnModelCreating, se necessário
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    base.OnModelCreating(modelBuilder);
-
-        //    // Adicionar configurações de mapeamento personalizadas se necessário
-        //    // Exemplo: Configuração de chaves primárias compostas ou índices
-        //    modelBuilder.Entity<Imoveis>(entity =>
-        //    {
-        //        entity.HasKey(e => e.ImovelId); // Define a chave primária
-
-        //        entity.Property(e => e.TipoImovel)
-        //              .HasMaxLength(255)
-        //              .IsRequired(); // Configurações adicionais como obrigatoriedade
-
-        //        entity.Property(e => e.Cep)
-        //              .HasMaxLength(255);
-
-        //        entity.Property(e => e.Condominio)
-        //              .HasColumnType("decimal(18,2)"); // Formato do decimal no banco de dados
-
-        //        entity.Property(e => e.ValorImovel)
-        //              .HasColumnType("decimal(18,2)");
-
-        //        entity.Property(e => e.Bairro)
-        //              .HasMaxLength(255);
-
-        //        entity.Property(e => e.Descricao);
-
-        //        entity.Property(e => e.Endereco)
-        //              .HasMaxLength(255);
-
-        //        entity.Property(e => e.Complemento)
-        //              .HasMaxLength(255);
-
-        //        // Adicionar outras configurações, índices ou relacionamentos se necessário
-        //    });
-        //}
+        public DbSet<Contratos> Contratos { get; set; }
     }
 }
