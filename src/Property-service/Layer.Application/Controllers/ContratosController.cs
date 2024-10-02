df2ac5300b9cf17eecf92fc9f7d54ef970d131d3
Matheus Ribeiro dos Santos <124204749+omatheu@users.noreply.github.com>
2024-10-01 21:21:32 -0300

    feat: adjustments in controller

--- src/Property-service/Layer.Application/Controllers/ContratosController.cs
+++ src/Property-service/Layer.Application/Controllers/ContratosController.cs
@@ -1,5 +1,6 @@
 ﻿using Layer.Domain.Entites;
 using Layer.Domain.Interfaces;
+using Layer.Services.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using property_management.Models;
@@ -11,10 +12,12 @@ namespace property_management.Controllers
     public class ContratosController : Controller
     {
         private readonly IContratosRepository _contratoService;
+        private readonly IimoveisRepository _imoveisService;
 
-        public ContratosController(IContratosRepository contratoService)
+        public ContratosController(IContratosRepository contratoService, IimoveisRepository imoveisService)
         {
             _contratoService = contratoService;
+            _imoveisService = imoveisService;
         }
 
         [HttpGet("PegarContratoPorId/{id}")]
@@ -35,10 +38,57 @@ namespace property_management.Controllers
             return Ok(contratos);
         }
 
-        [HttpPost]
-        public async Task<IActionResult> AddContrato([FromBody] NewContratos newContratos)
+        [HttpPost("CriarUmNovoContrato")]
+        public async Task<IActionResult> AddContrato([FromBody] NewContratos newContrato)
         {
-            throw new NotImplementedException();
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // Verificar se o LocadorId existe
+            //var locadorExists = await _locadoresService.LocadorExistsAsync(newContrato.LocadorId);
+            //if (!locadorExists)
+            //{
+            //    return NotFound(new { Message = $"Locador com ID {newContrato.LocadorId} não encontrado." });
+            //}
+
+            // Verificar se o LocatarioId existe
+            //var locatarioExists = await _locatariosService.LocatarioExistsAsync(newContrato.LocatarioId);
+            //if (!locatarioExists)
+            //{
+            //    return NotFound(new { Message = $"Locatário com ID {newContrato.LocatarioId} não encontrado." });
+            //}
+
+            //// Verificar se o ImovelId existe
+            //var imovelExists = await _imoveisService.GetByIdImoveisAsync(newContrato.ImovelId);
+            //if (!imovelExists)
+            //{
+            //    return NotFound(new { Message = $"Imóvel com ID {newContrato.ImovelId} não encontrado." });
+            //}
+
+            var contrato = new Contratos
+            {
+                LocadorId = newContrato.LocadorId,
+                LocatarioId = newContrato.LocatarioId,
+                ImovelId = newContrato.ImovelId,
+                ValorAluguel = newContrato.ValorAluguel,
+                Iptu = newContrato.Iptu,
+                TaxaAdm = newContrato.TaxaAdm,
+                DataInicio = newContrato.DataInicio,
+                DataEncerramento = newContrato.DataEncerramento,
+                TipoGarantia = newContrato.TipoGarantia,
+                CondicoesEspeciais = newContrato.CondicoesEspeciais,
+                Status = newContrato.Status,
+                DataPagamento = newContrato.DataPagamento,
+                DataRescisao = newContrato.DataRescisao,
+                Renovado = newContrato.Renovado,
+                DataEncerramentoRenovacao = newContrato.DataEncerramentoRenovacao,
+                ValorReajuste = newContrato.ValorReajuste
+            };
+
+            var novoContrato = await _contratoService.AddAsync(contrato);
+            return Ok(novoContrato);
         }
 
         [HttpPut("{id}")]
