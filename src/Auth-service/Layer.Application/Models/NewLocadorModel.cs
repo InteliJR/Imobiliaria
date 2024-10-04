e1fb126ade4bf8067445a2e01bfc4c9ccfaed23e
Rodrigo-07 <rodrigo.santos@sou.inteli.edu.br>
2024-10-04 12:43:40 -0300

    faet: logica dos locadores

---
+++ src/Auth-service/Layer.Application/Models/NewLocadorModel.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Layer.Application.Models
+{
+    public class NewLocadorModel
+    {
+        [Required]
+        [MaxLength(30)]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string CPF { get; set; }
+
+        [Required]
+        public int ImovelId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Nacionalidade { get; set; }
+
+        [Required]
+        public string NumeroTelefone { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string NomeCompletoLocador { get; set; }
+
+        [Required]
+        [MaxLength(18)]
+        public string CNPJ { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Endereco { get; set; }
+
+        [Required]
+        [MaxLength(25)]
+        public string Passaporte { get; set; }
+
+        [Required]
+        [MaxLength(25)]
+        public string RG { get; set; }
+    
+    }
+}
