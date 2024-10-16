f259f42d37ede7da1a0a8d1a44823e7e7dd51527
Rodrigo-07 <rodrigo.santos@sou.inteli.edu.br>
2024-10-16 13:14:34 -0300

    feat: envio da role e do ID no token JWT

--- src/Auth-service/Layer.Services/Services/TokenService.cs
+++ src/Auth-service/Layer.Services/Services/TokenService.cs
@@ -18,11 +18,13 @@ namespace Layer.Services.Services
     {
         private readonly JwtSettings _jwtSettings;
         private readonly IUserService _userService;
+        private readonly ILocadorService _locadorService;
 
-        public TokenService(JwtSettings jwtSettings, IUserService userService)
+        public TokenService(JwtSettings jwtSettings, IUserService userService, ILocadorService locadorService)
         {
             _jwtSettings = jwtSettings;
             _userService = userService;
+            _locadorService = locadorService;
         }
 
         public string GenerateToken(User user)
@@ -32,14 +34,32 @@ namespace Layer.Services.Services
                 throw new Exception("Usuário não existe.");
             }
 
-            var role = Roles.Admin.ToString(); // Pega o role do usuário !!!PUXAR DO BANCO DE DADOS!!!
+            string role = "";
+            int roleID = 0;
+
+            if (user.TipoUsuario.ToString() == Roles.Locador.ToString())
+            {
+                role += Roles.Locador.ToString();
+                roleID = _locadorService.GetLocadorByUserId(user.UsuarioId).Result.LocadorId;
+            }
+            else if (user.TipoUsuario.ToString() == Roles.Locatario.ToString())
+            {
+                role += Roles.Locatario.ToString();
+                roleID = _locadorService.GetLocadorByUserId(user.UsuarioId).Result.LocadorId;
+            }
+            else if (user.TipoUsuario.ToString() == Roles.Admin.ToString())
+            {
+                role += Roles.Admin.ToString();
+            }
+
 
             var claims = new List<Claim>
             {
                 // Lista de coisas/infos que o token vai ter
                 new Claim(JwtRegisteredClaimNames.Sub, user.Email),
                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-                new Claim(ClaimTypes.Role, role) // Adiciona o role no token
+                new Claim(ClaimTypes.Role, role), // Adiciona o role no token
+                new Claim("RoleID", roleID.ToString())
             };
 
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.SecretKey));
