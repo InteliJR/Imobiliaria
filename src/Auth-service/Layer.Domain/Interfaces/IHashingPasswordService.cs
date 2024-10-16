4b4724488c2ac61790ffe2124214cc9ca2d26fdd
Rodrigo-07 <rodrigo.santos@sou.inteli.edu.br>
2024-10-16 11:47:23 -0300

    feat: hash das senhas

---
+++ src/Auth-service/Layer.Domain/Interfaces/IHashingPasswordService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Layer.Domain.Interfaces
+{
+    public interface IHashingPasswordService
+    {
+        Task<string> HashPassword(string password);
+
+        Task<bool> VerifyPassword(string password, string HashedPassword);
+    }
+}
