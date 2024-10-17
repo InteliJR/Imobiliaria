d9de45ce90425611c7a9aae4c183ce74cf9f72ba
olin-med <olin.costa@sou.inteli.edu.br>
2024-10-17 16:42:57 -0300

    feat: adds footer component

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
 
 
 export default function App() {
   return (
     <div>
       <Navbar />
+      <Footer />
     </div>
   )
 }
