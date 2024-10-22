1a4e077020b762cbadeb6c3875e6cf10a7394895
Thiago Gomes <thiago.gomes@sou.inteli.edu.br>
2024-10-21 21:36:03 -0300

    feat: aplica os detalhes finais e adiciona uma estilização padrão para elementos main

--- src/frontend/src/pages/Landing.tsx
+++ src/frontend/src/pages/Landing.tsx
@@ -1,48 +1,39 @@
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer";
-
-// Importação das imagens
-import logo from "../assets/landingPage/logo.svg";
-import houseUp from "../assets/landingPage/houseUp.png";
-import houseDown from "../assets/landingPage/houseDown.png";
-import houseBg from "../assets/landingPage/houseBg.jpg"; // Importe a imagem aqui
+import WelcomeBanner from "../components/Landing/WelcomeBanner";
+import FeaturesSection from "../components/Landing/FeaturesSection";
+import logoSimplified from "../assets/landingPage/logoSimplified.svg";
 
 export default function Landing() {
   return (
     <div>
-      <Navbar />
+      <Navbar showLoginButton={true} />
 
-      <main className="flex flex-col gap-4 mx-4 my-6">
-        <div className="w-full flex justify-between items-start">
-          <img src={logo} className="w-36" alt="Imagem com a logotipo" />
-          <img src={houseUp} className="w-44" alt="Imagem estilizada de uma casa" />
+      <main className="main-custom">
+        <WelcomeBanner />
+
+        <FeaturesSection />
+
+        <div className="relative"> {/* Div para criar um container relativo e posicionar a logo simplificada (absolute)  */}
+          <p className="text-right leading-tight">
+            A plataforma que <span className="font-bold">centraliza</span> toda
+            a relação entre locador e locatário, simplificando a gestão de
+            imóveis, contratos e pagamentos em um só lugar
+          </p>
+
+          <img // Logo que fica espalhada pela tela
+            src={logoSimplified}
+            className="w-16 absolute -bottom-6 -left-6 z-0 -rotate-45 opacity-75"
+            alt="Logotipo simplificado da K.K. Administradora"
+          />
         </div>
 
-        <div className="w-full flex justify-between items-end">
-          <p>Bem vindo à <br /> <span className="font-bold">K.K. Administradora</span></p>
-          <img src={houseDown} className="w-44" alt="Imagem estilizada de uma casa" />
-        </div>
-
-        <div
-          className="w-full h-14 flex justify-center mt-2 bg-no-repeat bg-cover rounded-md"
-          style={{ backgroundImage: `url(${houseBg})`, backgroundPosition: 'center calc(100% + 40px)'  }} // Definindo o background com style
+        <button
+          className="w-full h-10 z-10 bg-gray-600 rounded-md"
+          aria-label="Entrar na plataforma"
         >
-          <div className="flex-1 flex justify-center items-center">
-            <p className="font-bold text-white">Agilidade</p>
-          </div>
-
-          <div className="flex-1 flex justify-center items-center border-l-2 border-r-2 border-white-900">
-            <p className="font-bold text-white">Segurança</p>
-          </div>
-
-          <div className="flex-1 flex justify-center items-center">
-            <p className="font-bold text-white">Simplicidade</p>
-          </div>
-        </div>
-
-        <p className="text-right leading-tight">A plataforma que <span className="font-bold">centraliza</span> toda a relação entre locador e locatário, simplificando a gestão de imóveis, contratos e pagamentos em um só lugar</p>
-
-        <button className="w-full h-10 bg-gray-600 rounded-md">Entrar</button>
+          Entrar
+        </button>
       </main>
 
       <Footer />
