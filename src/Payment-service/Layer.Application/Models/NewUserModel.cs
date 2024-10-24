2d470a1bc27289c37e9c2cbcaa8629c107772ab0
BretasArthur1 <arthur.bretas@sou.inteli.edu.br>
2024-10-23 22:59:17 -0300

    feat: Adiciona CRUD do servico de pagamentos

---
+++ src/Payment-service/Layer.Application/Models/NewUserModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Layer.Application.Models
+{
+    public class NewUserModel
+    {
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(255)]
+        public string Email { get; set; }
+
+    }
+}
