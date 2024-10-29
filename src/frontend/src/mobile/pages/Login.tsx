be4fbbad029edd4144d29f398503294a60513d63
olin-med <olin.costa@sou.inteli.edu.br>
2024-10-29 20:31:35 -0300

    fix: fixes Login page conflict

--- src/frontend/src/mobile/pages/Login.tsx
+++ src/frontend/src/mobile/pages/Login.tsx
@@ -4,36 +4,35 @@ import FormField from '../components/Form/FormField';
 
 export default function Login() {
   return (
-    <div className="bg-[#F0F0F0] flex flex-col min-h-screen">
-      {/* Navbar */}
+    <div>
       <NavbarLogin />
 
-      {/* Main Content */}
-      <div className="flex-grow flex flex-col justify-center items-center px-4">
-        <div className="w-full max-w-sm">
-          {/* Form */}
-          <h2 className="text-start text-title font-strong mb-10">Login</h2>
+      <main className="bg-[#F0F0F0] flex flex-col min-h-screen">
+        {/* Main Content */}
+        <div className="flex-grow flex flex-col justify-center items-center px-4">
+          <div className="w-full max-w-sm">
+            {/* Form */}
+            <h2 className="text-start text-title font-   mb-10">Login</h2>
 
-          <form className="">
-            <div className='mb-6'>
-              <FormField placeholder="Email" label="Email:"/>
-              
-            </div>
-            <div className='mb-10'>
-              <FormField placeholder="Password" label="Senha:"/>
-            </div>
-            {/* Submit Button */}
-            <button
-              type="submit"
-              className="w-full h-10 bg-neutral-900 text-neutral-50 font-bold rounded hover:bg-blue-700 transition-colors"
-            >
-              Login
-            </button>
-          </form>
+            <form>
+              <div className="mb-6">
+                <FormField placeholder="Email" label="Email:" />
+              </div>
+              <div className="mb-10">
+                <FormField placeholder="Password" label="Senha:" />
+              </div>
+              {/* Submit Button */}
+              <button
+                type="submit"
+                className="w-full h-10 bg-neutral-900 text-neutral-50 rounded hover:bg-blue-700 transition-colors"
+              >
+                Login
+              </button>
+            </form>
+          </div>
         </div>
-      </div>
+      </main>
 
-      {/* Footer */}
       <Footer />
     </div>
   );
