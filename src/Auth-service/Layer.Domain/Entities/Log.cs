648a8d5650fdcd8b5e57af50ffcc9150c6160bd4
Rodrigo-07 <rodrigo.santos@sou.inteli.edu.br>
2024-10-30 01:38:10 -0300

    feat: mongo DB

---
+++ src/Auth-service/Layer.Domain/Entities/Log.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Layer.Domain.Entities
+{
+    public class Log
+    {
+        public string Message { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
