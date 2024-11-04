6290d7d7f31b9528246f02b87b258238d92d035e
renan-feitosa <renan.oliveira@sou.inteli.edu.br>
2024-11-04 20:31:48 -0300

    feat: add profile page fields

--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -13,6 +13,13 @@ export default {
       colors: {
         'neutral-black': '#1F1E1C',
         'yellow-middle': '#F2B705',
+        'yellow-dark': '#BF7E04',
+        'yellow-darker': '#593202',
+        
+        'neutral-900': '#363430',
+        'neutral-600': '#76726A',
+        'neutral-300': '#B2AFA9',
+
       },
       fontFamily: {
         sans: ['Object Sans', 'sans-serif'], // Custom font
