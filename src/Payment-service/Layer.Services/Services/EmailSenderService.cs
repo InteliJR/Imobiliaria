ac95713e068b1de0721cc8498dac3c594143d088
BretasArthur1 <arthur.bretas@sou.inteli.edu.br>
2024-11-04 20:48:35 -0300

    feat: adiciona logica de envio automatico de email atraves de um backgroundservice

--- src/Payment-service/Layer.Services/Services/EmailSenderService.cs
+++ src/Payment-service/Layer.Services/Services/EmailSenderService.cs
@@ -11,36 +11,36 @@ namespace Layer.Services.Services
     public class EmailSenderService : IEmailSender
     {
     public async Task<string> SendEmailAsync(string emailRecipient, string subject, string body)
-    {
-        try
-            {
-            var sender = "testeimob178@gmail.com";
-            var senderPassword = "ocnf lrwc xmit pags"; // Usado apenas para essa aplicação
-
-            using (SmtpClient client = new SmtpClient("smtp.gmail.com", 587))
+        {
+            try
                 {
-                    client.EnableSsl = true;
-                    client.UseDefaultCredentials = false;
-                    client.Credentials = new NetworkCredential(sender, senderPassword);
+                var sender = "testeimob178@gmail.com";
+                var senderPassword = "ocnf lrwc xmit pags"; // Usado apenas para essa aplicação
 
-                    MailMessage mailMessage = new MailMessage
-                {
-                    From = new MailAddress(sender),
-                    Subject = subject,
-                    Body = body,
-                    IsBodyHtml = true
-                };
-                    mailMessage.To.Add(emailRecipient);
+                using (SmtpClient client = new SmtpClient("smtp.gmail.com", 587))
+                    {
+                        client.EnableSsl = true;
+                        client.UseDefaultCredentials = false;
+                        client.Credentials = new NetworkCredential(sender, senderPassword);
 
-                    await client.SendMailAsync(mailMessage);
+                        MailMessage mailMessage = new MailMessage
+                    {
+                        From = new MailAddress(sender),
+                        Subject = subject,
+                        Body = body,
+                        IsBodyHtml = true
+                    };
+                        mailMessage.To.Add(emailRecipient);
+
+                        await client.SendMailAsync(mailMessage);
+                    }
+
+                return "E-mail enviado com sucesso!";
                 }
-
-            return "E-mail enviado com sucesso!";
-            }
-            catch (Exception ex)
-            {
-            throw new Exception("Erro ao enviar o e-mail: " + ex.Message);
-            }
+                catch (Exception ex)
+                {
+                throw new Exception("Erro ao enviar o e-mail: " + ex.Message);
+                }
         }
     }
 
