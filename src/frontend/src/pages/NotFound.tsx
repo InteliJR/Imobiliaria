bbe809ec340744206e9a8deed5b56095cb7704d6
renan-feitosa <renan.oliveira@sou.inteli.edu.br>
2024-11-04 21:09:17 -0300

    fix: color changed

--- src/frontend/src/pages/NotFound.tsx
+++ src/frontend/src/pages/NotFound.tsx
@@ -4,12 +4,12 @@ import Navbar from '../components/Navbar';
 const NotFound = () => (
     <div>
         <Navbar />
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-800">
-            <h1 className="text-9xl font-extrabold text-neutral-900 mb-4">404</h1>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-yellow-darker">
+            <h1 className="text-9xl font-extrabold mb-4">404</h1>
             <p className="text-2xl md:text-3xl font-semibold mb-6">
                 Oops! Página não encontrada.
             </p>
-            <p className="text-gray-600 mb-8">
+            <p className="text-gray-600 mb-14">
                 Parece que a página que você está tentando acessar não existe.
             </p>
             <Link
