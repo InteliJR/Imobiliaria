6f0659f502ea13e4edb273c31b1550bf3570e20b
lucas-nunes-matheus <lucas.matheus@sou.inteli.edu.br>
2024-11-06 21:23:34 -0300

    refactor: corrige a rota mockada de redirecionamento no hook useAuth

--- src/frontend/src/hooks/useAuth.ts
+++ src/frontend/src/hooks/useAuth.ts
@@ -9,9 +9,9 @@ const useAuth = (requiredRole: string) => {
   // verifica se o role do user é o mesmo role requerido para a página, se não for, redireciona para unauthorized
   useEffect(() => {
     if (!userRole || userRole !== requiredRole) {
-      navigate('/unauthorized');
+      navigate('/');   // !! Trocar '/' por uma rota como '/notfound' ou '/unauthorized'  
     }
   }, [requiredRole, userRole, navigate]);
 };
 
-export default useAuth;
+export default useAuth; // !! importe e chame o metodo "useAuth('Role') para verificar a permissao de uma role para visualizar a page
