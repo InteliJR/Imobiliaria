56342a79c42fbe49c8718f6603efbb0c69aa0efe
Thiago Gomes <thiago.gomes@sou.inteli.edu.br>
2024-11-18 18:33:48 -0300

    feat: adiciona e importa as fontes corretamente

--- src/frontend/src/desktop/pages/Landing.tsx
+++ src/frontend/src/desktop/pages/Landing.tsx
@@ -1,9 +1,12 @@
+import { useNavigate } from "react-router-dom";
 import FeaturesSection from "../../components/Landing/FeaturesSection";
 import Navbar from "../../mobile/components/Navbar/Navbar";
 import Footer from "../../components/Footer/FooterBig";
 import Botao from "../../components/Botoes/Botao";
 
 export default function Landing() {
+  const navigate = useNavigate();
+
   return (
     <main className="main-custom">
       <Navbar />
@@ -16,10 +19,10 @@ export default function Landing() {
         </div>
 
 
-        <div className="flex justify-center items-center w-full gap-4 my-4">
+        <div className="flex justify-center items-center w-full gap-4 my-10">
         <FeaturesSection />
           <p className="w-[52rem] 2xl:w-[120rem] text-right leading-tight max-w-2xl">
-            A plataforma que <span className="font-bold">centraliza</span>
+            A plataforma que <span className="font-bold">centraliza </span>
             toda a relação entre locador e locatário, simplificando a gestão de
             imóveis, contratos e pagamentos em um só lugar
           </p>
