eeffe89ad385b254ef310adb1ee3e16ecf1fa566
Thiago Gomes <thiago.gomes@sou.inteli.edu.br>
2024-12-05 21:26:26 -0300

    feat: cria botao de logout e faz ajustes estéticos

--- src/frontend/src/mobile/components/Navbar/Menu.tsx
+++ src/frontend/src/mobile/components/Navbar/Menu.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { MdClose } from "react-icons/md";
+import { BiLogOut } from "react-icons/bi";
 
 interface MenuProps {
   userType: string;
@@ -12,87 +13,148 @@ const Menu: React.FC<MenuProps> = ({ userType, toggleMenu, isOpen }) => {
   const navigate = useNavigate();
   const menuRef = useRef<HTMLDivElement>(null);
   const [menuStyle, setMenuStyle] = useState<React.CSSProperties>({
-    transform: "translateX(-100%)", // Começa fora da tela
+    transform: "translateX(-100%)",
     transition: "transform 0.5s ease-in-out",
   });
 
-  // Atualiza o estilo do menu com base no estado isOpen
+  const [isClosing, setIsClosing] = useState(false);
+
   useEffect(() => {
-    if (isOpen) {
+    if (isOpen && !isClosing) {
       setMenuStyle({
-        transform: "translateX(0)", // Entra na tela
+        transform: "translateX(0)",
+        transition: "transform 0.5s ease-in-out",
+      });
+    } else if (isClosing) {
+      setMenuStyle({
+        transform: "translateX(-100%)",
         transition: "transform 0.5s ease-in-out",
       });
     } else {
       setMenuStyle({
-        transform: "translateX(-100%)", // Sai da tela
-        transition: "transform 0.5s ease-in-out",
+        transform: "translateX(-100%)",
+        transition: "none",
       });
     }
-  }, [isOpen]);
+  }, [isOpen, isClosing]);
 
-  // Fecha o menu ao clicar fora
   const handleClickOutside = (event: React.MouseEvent) => {
     if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+      handleToggleMenu();
+    }
+  };
+
+  const handleToggleMenu = () => {
+    if (isOpen) {
+      setIsClosing(true);
+      setTimeout(() => {
+        toggleMenu();
+        setIsClosing(false);
+      }, 500);
+    } else {
       toggleMenu();
     }
   };
 
-  // Renderiza os itens do menu com base no tipo de usuário
+  const handleLogout = () => {
+    // lógica de logout
+    console.log("sair da conta")
+    navigate("/");
+  };
+
+  const redirect = (url: string) => {
+    navigate(url);
+    handleToggleMenu();
+  };
+
   const renderMenuItems = () => {
     switch (userType) {
       case "Administrador":
         return (
           <>
-            <li>
-              <button onClick={() => navigate("/home")}>Início</button>
-            </li>
-            <li>
-              <button onClick={() => navigate("/dashboard")}>
-                Dashboard Administrativo Financeiro
+            <li className="w-full h-16 hover:h-20 hover:bg-[#181716] transition-all duration-300 ease-in-out">
+              <button
+                className="w-full h-full h-10 drop-shadow-[0_0px_10px_rgba(255,255,255,.5)]"
+                onClick={() => redirect("/home")}
+              >
+                Início
               </button>
             </li>
-            <li>
-              <button onClick={() => navigate("/gestao-usuarios")}>
+            <li className="w-full h-16 hover:h-20 hover:bg-[#181716] transition-all duration-300 ease-in-out">
+              <button
+                className="w-full h-full h-10 drop-shadow-[0_0px_10px_rgba(255,255,255,.5)]"
+                onClick={() => redirect("/dashboard")}
+              >
+                Dashboard
+              </button>
+            </li>
+            <li className="w-full h-16 hover:h-20 hover:bg-[#181716] transition-all duration-300 ease-in-out">
+              <button
+                className="w-full h-full h-10 drop-shadow-[0_0px_10px_rgba(255,255,255,.5)]"
+                onClick={() => redirect("/gestao-usuarios")}
+              >
                 Gestão de Usuários
               </button>
             </li>
-            <li>
-              <button onClick={() => navigate("/lista-chamados")}>
+            <li className="w-full h-16 hover:h-20 hover:bg-[#181716] transition-all duration-300 ease-in-out">
+              <button
+                className="w-full h-full h-10 drop-shadow-[0_0px_10px_rgba(255,255,255,.5)]"
+                onClick={() => redirect("/lista-chamados")}
+              >
                 Lista de chamados
               </button>
             </li>
-            <li>
-              <button onClick={() => navigate("/cadastrar-imoveis")}>
-                Cadastrar imóveis
+            <li className="w-full h-16 hover:h-20 hover:bg-[#181716] transition-all duration-300 ease-in-out">
+              <button
+                className="w-full h-full h-10 drop-shadow-[0_0px_10px_rgba(255,255,255,.5)]"
+                onClick={() => redirect("/cadastrar-imoveis")}
+              >
+                Cadastrar imóvel
               </button>
             </li>
-            <li>
-              <button onClick={() => navigate("/adicionar-contrato")}>
+            <li className="w-full h-16 hover:h-20 hover:bg-[#181716] transition-all duration-300 ease-in-out">
+              <button
+                className="w-full h-full h-10 drop-shadow-[0_0px_10px_rgba(255,255,255,.5)]"
+                onClick={() => redirect("/adicionar-contrato")}
+              >
                 Adicionar Contrato
               </button>
             </li>
-            <li>
-              <button onClick={() => navigate("/perfil")}>Perfil</button>
+            <li className="w-full h-16 hover:h-20 hover:bg-[#181716] transition-all duration-300 ease-in-out">
+              <button
+                className="w-full h-full h-10 drop-shadow-[0_0px_10px_rgba(255,255,255,.5)]"
+                onClick={() => redirect("/perfil")}
+              >
+                Perfil
+              </button>
             </li>
           </>
         );
       case "Jurídico":
         return (
           <>
-            <li>
-              <button onClick={() => navigate("/dashboard-financeiro")}>
+            <li className="w-full h-16 hover:h-20 hover:bg-[#181716] transition-all duration-300 ease-in-out">
+              <button
+                className="w-full h-full h-10 drop-shadow-[0_0px_10px_rgba(255,255,255,.5)]"
+                onClick={() => redirect("/dashboard-financeiro")}
+              >
                 Dashboard Financeiro
               </button>
             </li>
-            <li>
-              <button onClick={() => navigate("/lista-imoveis")}>
-                Lista Imóveis
+            <li className="w-full h-16 hover:h-20 hover:bg-[#181716] transition-all duration-300 ease-in-out">
+              <button
+                className="w-full h-full h-10 drop-shadow-[0_0px_10px_rgba(255,255,255,.5)]"
+                onClick={() => redirect("/lista-imoveis")}
+              >
+                Lista de Imóveis
               </button>
             </li>
-            <li>
-              <button onClick={() => navigate("/documentos-locacao")}>
-                Documentos relacionados às locações
+            <li className="w-full h-16 hover:h-20 hover:bg-[#181716] transition-all duration-300 ease-in-out">
+              <button
+                className="w-full h-full h-10 drop-shadow-[0_0px_10px_rgba(255,255,255,.5)]"
+                onClick={() => redirect("/documentos-locacao")}
+              >
+                Documentos
               </button>
             </li>
           </>
@@ -100,24 +162,43 @@ const Menu: React.FC<MenuProps> = ({ userType, toggleMenu, isOpen }) => {
       case "Locador":
         return (
           <>
-            <li>
-              <button onClick={() => navigate("/home")}>Início</button>
+            <li className="w-full h-16 hover:h-20 hover:bg-[#181716] transition-all duration-300 ease-in-out">
+              <button
+                className="w-full h-full h-10 drop-shadow-[0_0px_10px_rgba(255,255,255,.5)]"
+                onClick={() => redirect("/home")}
+              >
+                Início
+              </button>
             </li>
-            <li>
-              <button onClick={() => navigate("/perfil")}>Perfil</button>
+            <li className="w-full h-16 hover:h-20 hover:bg-[#181716] transition-all duration-300 ease-in-out">
+              <button
+                className="w-full h-full h-10 drop-shadow-[0_0px_10px_rgba(255,255,255,.5)]"
+                onClick={() => redirect("/perfil")}
+              >
+                Perfil
+              </button>
             </li>
-            <li>
-              <button onClick={() => navigate("/visualizar-imoveis")}>
+            <li className="w-full h-16 hover:h-20 hover:bg-[#181716] transition-all duration-300 ease-in-out">
+              <button
+                className="w-full h-full h-10 drop-shadow-[0_0px_10px_rgba(255,255,255,.5)]"
+                onClick={() => redirect("/visualizar-imoveis")}
+              >
                 Visualizar Imóveis
               </button>
             </li>
-            <li>
-              <button onClick={() => navigate("/dashboard-financeiro")}>
+            <li className="w-full h-16 hover:h-20 hover:bg-[#181716] transition-all duration-300 ease-in-out">
+              <button
+                className="w-full h-full h-10 drop-shadow-[0_0px_10px_rgba(255,255,255,.5)]"
+                onClick={() => redirect("/dashboard-financeiro")}
+              >
                 Dashboard Financeiro
               </button>
             </li>
-            <li>
-              <button onClick={() => navigate("/documentos-locacao")}>
+            <li className="w-full h-16 hover:h-20 hover:bg-[#181716] transition-all duration-300 ease-in-out">
+              <button
+                className="w-full h-full h-10 drop-shadow-[0_0px_10px_rgba(255,255,255,.5)]"
+                onClick={() => redirect("/documentos-locacao")}
+              >
                 Documentos das locações
               </button>
             </li>
@@ -126,30 +207,52 @@ const Menu: React.FC<MenuProps> = ({ userType, toggleMenu, isOpen }) => {
       case "Locatário":
         return (
           <>
-            <li>
-              <button onClick={() => navigate("/home")}>Início</button>
-            </li>
-            <li>
-              <button onClick={() => navigate("/perfil")}>Perfil</button>
-            </li>
-            <li>
-              <button onClick={() => navigate("/abertura-chamados")}>
-                Abertura de chamados
+            <li className="w-full h-16 hover:h-20 hover:bg-[#181716] transition-all duration-300 ease-in-out">
+              <button
+                className="w-full h-full h-10 drop-shadow-[0_0px_10px_rgba(255,255,255,.5)]"
+                onClick={() => redirect("/home")}
+              >
+                Início
               </button>
             </li>
-            <li>
-              <button onClick={() => navigate("/visualizar-imoveis")}>
+            <li className="w-full h-16 hover:h-20 hover:bg-[#181716] transition-all duration-300 ease-in-out">
+              <button
+                className="w-full h-full h-10 drop-shadow-[0_0px_10px_rgba(255,255,255,.5)]"
+                onClick={() => redirect("/perfil")}
+              >
+                Perfil
+              </button>
+            </li>
+            <li className="w-full h-16 hover:h-20 hover:bg-[#181716] transition-all duration-300 ease-in-out">
+              <button
+                className="w-full h-full h-10 drop-shadow-[0_0px_10px_rgba(255,255,255,.5)]"
+                onClick={() => redirect("/abertura-chamados")}
+              >
+                Abrir chamado
+              </button>
+            </li>
+            <li className="w-full h-16 hover:h-20 hover:bg-[#181716] transition-all duration-300 ease-in-out">
+              <button
+                className="w-full h-full h-10 drop-shadow-[0_0px_10px_rgba(255,255,255,.5)]"
+                onClick={() => redirect("/visualizar-imoveis")}
+              >
                 Visualizar Imóveis
               </button>
             </li>
-            <li>
-              <button onClick={() => navigate("/dashboard-financeiro")}>
-                Dashboard Financeiro
+            <li className="w-full h-16 hover:h-20 hover:bg-[#181716] transition-all duration-300 ease-in-out">
+              <button
+                className="w-full h-full h-10 drop-shadow-[0_0px_10px_rgba(255,255,255,.5)]"
+                onClick={() => redirect("/dashboard-financeiro")}
+              >
+                Dashboard
               </button>
             </li>
-            <li>
-              <button onClick={() => navigate("/documentos-locacao")}>
-                Documentos das locações
+            <li className="w-full h-16 hover:h-20 hover:bg-[#181716] transition-all duration-300 ease-in-out">
+              <button
+                className="w-full h-full h-10 drop-shadow-[0_0px_10px_rgba(255,255,255,.5)]"
+                onClick={() => redirect("/documentos-locacao")}
+              >
+                Documentos
               </button>
             </li>
           </>
@@ -161,24 +264,40 @@ const Menu: React.FC<MenuProps> = ({ userType, toggleMenu, isOpen }) => {
 
   return (
     <div
-      className={`fixed inset-0 z-50 bg-black bg-opacity-60 ${
+      className={`fixed inset-0 z-50 bg-black bg-opacity-40 ${
         isOpen ? "opacity-100" : "opacity-0 pointer-events-none"
       } transition-opacity duration-300`}
       onClick={handleClickOutside}
     >
-      <div
+      <nav
         ref={menuRef}
-        style={menuStyle} // Aplica o estilo inline para a animação
-        className="bg-white text-black h-full w-[70%] p-8 shadow-2xl rounded-r-lg"
+        style={menuStyle}
+        className="flex flex-col justify-between bg-neutral-black text-[#fefefe] h-full w-[60%] shadow-2xl rounded-r-lg"
       >
-        <ul className="space-y-6 text-center text-xl">{renderMenuItems()}</ul>
-        <button
-          onClick={toggleMenu}
-          className="absolute top-4 right-4 text-black text-3xl focus:outline-none"
-        >
-          <MdClose />
-        </button>
-      </div>
+        <ul className="flex flex-col justify-between text-center text-xl  mt-3">
+          {renderMenuItems()}
+        </ul>
+        <div className="flex items-center w-full h-16">
+          <button
+            onClick={handleLogout}
+            className="flex items-center justify-center flex-1 hover:-translate-y-1 transition-all duration-200 ease-in-out"
+            title="Sair da Conta"
+          >
+            <BiLogOut color="#fefefe" size={35} />
+          </button>
+
+          {/* linha vertical decorativa */}
+          <div className="w-px h-4/5 bg-[#fefefe] drop-shadow-[0_0px_10px_rgba(255,255,255,.5)]" />
+
+          <button
+            onClick={handleToggleMenu}
+            className="flex items-center justify-center flex-1 hover:-translate-y-1 transition-all duration-200 ease-in-out"
+            title="Fechar Menu"
+          >
+            <MdClose color="#fefefe" size={35} />
+          </button>
+        </div>
+      </nav>
     </div>
   );
 };
