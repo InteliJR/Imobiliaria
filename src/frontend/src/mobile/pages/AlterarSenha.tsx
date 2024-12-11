9ff199d90db5fac916735896c7cf05c2f3aca388
Thiago Gomes <thiago.gomes@sou.inteli.edu.br>
2024-12-11 16:39:47 -0300

    feat: adiciona componente de carregamento na página de alteração de senha

--- src/frontend/src/mobile/pages/AlterarSenha.tsx
+++ src/frontend/src/mobile/pages/AlterarSenha.tsx
@@ -4,6 +4,7 @@ import Footer from "../../components/Footer/FooterSmall";
 import Voltar from "../components/Voltar";
 import FormField from "../components/Form/FormField";
 import Botao from "../../components/Botoes/Botao";
+import Loading from "../../components/Loading";
 import ModalConfirmacao from "../components/ModalConfirmacao";
 import { showSuccessToast, showErrorToast } from "../../utils/toastMessage";
 import axiosInstance from "../../services/axiosConfig";
@@ -13,6 +14,7 @@ export default function Senha() {
   const [novaSenha, setNovaSenha] = useState("");
   const [confirmarSenha, setConfirmarSenha] = useState("");
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [loading, setLoading] = useState(false); // estado para controlar o componente de carregamento
   const [result, setResult] = useState("");
 
   // Função para exibir o modal
@@ -23,38 +25,37 @@ export default function Senha() {
   // Função chamada ao confirmar a ação no modal
   const handleConfirm = async () => {
     setIsModalVisible(false);
-    // console.log("Senha alterada com sucesso:", {
-    //   senhaAntiga,
-    //   novaSenha,
-    //   confirmarSenha,
-    // });
-
-    // Lógica de integração com o back
-
-    try{
-
-      if(senhaAntiga === "" || novaSenha === "" || confirmarSenha === "") {
+    setLoading(true);
+    try {
+      if (senhaAntiga === "" || novaSenha === "" || confirmarSenha === "") {
         console.log("Preencha todos os campos");
         return;
       }
 
-      if(novaSenha !== confirmarSenha) {
+      if (novaSenha !== confirmarSenha) {
         console.log("As senhas não coincidem");
         return;
       }
 
-      const response = await axiosInstance.post(`auth/User/AlterarSenhaUsuario?oldPassword=${senhaAntiga}&newPassword=${confirmarSenha}`);
+      const response = await axiosInstance.post(
+        `auth/User/AlterarSenhaUsuario?oldPassword=${senhaAntiga}&newPassword=${confirmarSenha}`
+      );
 
       console.log(response.data);
 
       setResult("Senha alterada com sucesso");
-      showSuccessToast(response?.data?.message || "Senha alterada com sucesso.");
-      
-    } catch(error: any){
-      showErrorToast(error?.response?.data?.message || "Erro ao se conectar com o servidor.");
+      showSuccessToast(
+        response?.data?.message || "Senha alterada com sucesso."
+      );
+    } catch (error: any) {
+      showErrorToast(
+        error?.response?.data?.message || "Erro ao se conectar com o servidor."
+      );
 
-      console.log(error?.response?.data?.message || "Erro ao alterar a senha");
+      console.error(error?.response?.data?.message || "Erro ao alterar a senha");
       setResult(error?.response?.data?.message || "Erro ao alterar a senha");
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -77,11 +78,11 @@ export default function Senha() {
             onChange={setSenhaAntiga}
             isPassword
           />
-          <FormField 
-            label="Nova Senha" 
+          <FormField
+            label="Nova Senha"
             value={novaSenha}
-            onChange={setNovaSenha} 
-            isPassword 
+            onChange={setNovaSenha}
+            isPassword
           />
           <FormField
             label="Confirmar Nova Senha"
@@ -95,6 +96,8 @@ export default function Senha() {
 
         <Botao label="Confirmar" onClick={showModal} />
       </section>
+
+      {loading && <Loading type="spinner" />}
 
       <Footer />
 
