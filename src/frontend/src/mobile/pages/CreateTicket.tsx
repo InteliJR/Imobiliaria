9f9d50bbfea0ea127db1c155258e132812fa274a
Thiago Gomes <thiago.gomes@sou.inteli.edu.br>
2024-12-11 16:29:05 -0300

    feat: adiciona componente de carregamento na página de criação de chamado, adiciona botão de voltar e rodapé, além de reestruturar a página

--- src/frontend/src/mobile/pages/CreateTicket.tsx
+++ src/frontend/src/mobile/pages/CreateTicket.tsx
@@ -1,16 +1,20 @@
 import { useState, useEffect } from "react";
-import Navbar from '../components/Navbar/Navbar';
-import FormField from '../components/Form/FormField';
+import Navbar from "../components/Navbar/Navbar";
+import FormField from "../components/Form/FormField";
+import Footer from "../../components/Footer/FooterSmall";
+import Voltar from "../components/Voltar";
+import Loading from "../../components/Loading";
 import { showSuccessToast, showErrorToast } from "../../utils/toastMessage";
 import axiosInstance from "../../services/axiosConfig";
 import getTokenData from "../../services/tokenConfig";
 
 export default function CreateTicket() {
-  const [property, setProperty] = useState('');
+  const [property, setProperty] = useState("");
   const [ticketType, setTicketType] = useState("");
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
   const [houseNames, setHouseNames] = useState("");
+  const [loading, setLoading] = useState(false); // estado para controlar o componente de carregamento
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -20,8 +24,9 @@ export default function CreateTicket() {
       return;
     }
 
-    try {
+    setLoading(true);
 
+    try {
       // /Chamados/CriarUmNovoChamado
       // {
       //   "idImovel": 0,
@@ -32,7 +37,6 @@ export default function CreateTicket() {
       //   "tipoChamado": "string",
       //   "status": "string"
       // }
-
 
       const tokenData = getTokenData();
 
@@ -47,92 +51,117 @@ export default function CreateTicket() {
         tipoChamado: ticketType,
       };
 
+      const response = await axiosInstance.post(
+        "property/Chamados/CriarUmNovoChamado",
+        data
+      );
 
-      const response = await axiosInstance.post('property/Chamados/CriarUmNovoChamado', data);
-
-      console.log(response.data);
+      // Limpa formulário
+      setProperty("");
+      setTicketType("");
+      setTitle("");
+      setDescription("");
+      setHouseNames("");
 
       showSuccessToast("Chamado aberto com sucesso!");
-
     } catch (error) {
-      console.error('Error creating ticket:', error);
+      console.error("Error creating ticket:", error);
       showErrorToast(
-        error instanceof Error ? error.message : "Erro ao se conectar com o servidor."
+        error instanceof Error
+          ? error.message
+          : "Erro ao se conectar com o servidor."
       );
+    } finally {
+      setLoading(false);
     }
   };
 
   const allHousesNames = async () => {
-
-    try{
-
-      const imoveisResponse = await axiosInstance.get('property/Imoveis/PegarTodosImoveis');
+    try {
+      const imoveisResponse = await axiosInstance.get(
+        "property/Imoveis/PegarTodosImoveis"
+      );
 
       const imoveis = imoveisResponse.data;
 
-      console.log(imoveis)
-      
+      console.log(imoveis);
 
-      const houseNamesArray = imoveis.map((imovel: { endereco: any; }) => imovel.endereco);
+      const houseNamesArray = imoveis.map(
+        (imovel: { endereco: any }) => imovel.endereco
+      );
       setHouseNames(houseNamesArray.join("; "));
 
-    console.log(houseNamesArray);
-    const houseDictionary = imoveis.reduce((acc: { [key: string]: string }, imovel: { imovelId: string; endereco: string }) => {
-      acc[imovel.imovelId] = imovel.endereco;
-      return acc;
-    }, {});
+      console.log(houseNamesArray);
+      const houseDictionary = imoveis.reduce(
+        (
+          acc: { [key: string]: string },
+          imovel: { imovelId: string; endereco: string }
+        ) => {
+          acc[imovel.imovelId] = imovel.endereco;
+          return acc;
+        },
+        {}
+      );
 
-    console.log(houseDictionary);
+      console.log(houseDictionary);
 
-    setHouseNames(houseDictionary)
-    
-    } catch(error){
-      console.error('Error getting the houses:', error);
-
+      setHouseNames(houseDictionary);
+    } catch (error) {
+      console.error("Error getting the houses:", error);
     }
-  }
+  };
 
   useEffect(() => {
     allHousesNames();
   }, []);
 
   return (
-    <div>
+    <main className="main-custom">
       <Navbar />
-      <div className="mx-10 mt-10">
-        <h1 className="text-xl font-bold text-yellow-darker mb-6">Novo Chamado</h1>
-        <div className="min-h-screen flex flex-col items-center">
-          <div className="w-full max-w-xl py-6 bg-white rounded-lg">
+      <section className="section-custom">
+        <Voltar />
+        <h1 className="text-xl font-bold">Novo Chamado</h1>
+        <div className="flex flex-col items-center">
+          <div className="w-full max-w-xl rounded-lg">
             <form onSubmit={handleSubmit} className="space-y-4">
-
               {/* Seleção de Imóvel */}
               <div>
-                <label className="font-sans font-normal text-form-label text-neutral-900 mb-1.5">Imóvel</label>
+                <label className="font-sans font-normal text-form-label text-neutral-900 mb-1.5">
+                  Imóvel
+                </label>
                 <select
                   value={property}
                   onChange={(e) => setProperty(e.target.value)}
                   className={`h-10 w-full ${
-                    property ? 'bg-transparent border border-black' : 'bg-[#D9D9D9]'
+                    property
+                      ? "bg-transparent border border-black"
+                      : "bg-[#D9D9D9]"
                   } focus:outline-none px-2 text-form-label placeholder:text-form-label placeholder:text-black/60 rounded`}
                 >
                   <option value="" disabled>
                     Selecione um imóvel
                   </option>
                   <option value="">Selecione um imóvel</option>
-                    {Object.entries(houseNames).map(([id, address]) => (
-                    <option key={id} value={id}>{address}</option>
-                    ))}
+                  {Object.entries(houseNames).map(([id, address]) => (
+                    <option key={id} value={id}>
+                      {address}
+                    </option>
+                  ))}
                 </select>
               </div>
 
               {/* Seleção de Tipo */}
               <div>
-                <label className="font-sans font-normal text-form-label text-neutral-900 mb-1.5">Tipo</label>
+                <label className="font-sans font-normal text-form-label text-neutral-900 mb-1.5">
+                  Tipo
+                </label>
                 <select
                   value={ticketType}
                   onChange={(e) => setTicketType(e.target.value)}
                   className={`h-10 w-full ${
-                    ticketType ? 'bg-transparent border border-black' : 'bg-[#D9D9D9]'
+                    ticketType
+                      ? "bg-transparent border border-black"
+                      : "bg-[#D9D9D9]"
                   } focus:outline-none px-2 text-form-label placeholder:text-form-label placeholder:text-black/60 rounded`}
                 >
                   <option value="" disabled>
@@ -146,21 +175,25 @@ export default function CreateTicket() {
               </div>
 
               {/* Campo de Título */}
-              <FormField 
-                label="Título" 
-                value={title} 
-                onChange={setTitle} 
-                placeholder="Título do chamado" 
+              <FormField
+                label="Título"
+                value={title}
+                onChange={setTitle}
+                placeholder="Título do chamado"
               />
 
               {/* Campo de Descrição */}
               <div>
-                <label className="font-sans font-normal text-form-label text-neutral-900 mb-1.5">Descrição</label>
+                <label className="font-sans font-normal text-form-label text-neutral-900 mb-1.5">
+                  Descrição
+                </label>
                 <textarea
                   value={description}
                   onChange={(e) => setDescription(e.target.value)}
                   className={`h-20 w-full ${
-                    description ? 'bg-transparent border border-black' : 'bg-[#D9D9D9]'
+                    description
+                      ? "bg-transparent border border-black"
+                      : "bg-[#D9D9D9]"
                   } focus:outline-none px-2 text-form-label placeholder:text-form-label placeholder:text-black/60 rounded`}
                   rows={10}
                   placeholder="Descreva o problema ou solicitação"
@@ -177,7 +210,9 @@ export default function CreateTicket() {
             </form>
           </div>
         </div>
-      </div>
-    </div>
+      </section>
+      {loading && <Loading type="spinner" />}
+      <Footer />
+    </main>
   );
 }
