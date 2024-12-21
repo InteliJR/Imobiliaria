be1169e2997a661d7f23fa9c358e16a2fd795331
Thiago Gomes <thiago.gomes@sou.inteli.edu.br>
2024-12-21 18:12:50 -0300

    feat: altera o serviço apontado a fim de exemplificação

--- src/frontend/src/services/apiService.ts
+++ src/frontend/src/services/apiService.ts
@@ -3,7 +3,7 @@ const services = {
     authService: 'https://gateway-2ev7.onrender.com/auth',
     propertyService: 'https://gateway-2ev7.onrender.com/property',
     userService: 'https://gateway-2ev7.onrender.com/user', // adicionado para o esLint parar de gritar na página de criação de contrato
-    contractService: 'https://gateway-2ev7.onrender.com/user' // adicionado para o esLint parar de gritar na página de criação de contrato
+    contractService: 'https://gateway-2ev7.onrender.com/contract' // adicionado para o esLint parar de gritar na página de criação de contrato
 };
 
 export const getServiceUrl = (serviceName: keyof typeof services, path: string): string => {
