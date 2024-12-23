a044089301a53e7c2ab52a066b80058de813fafc
Matheus R. dos Santos <matheus.ribeiro@sou.inteli.edu.br>
2024-12-23 17:07:35 -0300

    fix: issues for build frontend

--- src/frontend/src/mobile/pages/CreateContract.tsx
+++ src/frontend/src/mobile/pages/CreateContract.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import FormField from "../../desktop/components/Form/FormField";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/FooterSmall";
