18e8a42d19fd4de83c90a02dde0337ac41940759
Rodrigo-07 <freirerodrigosales@gmail.com>
2025-01-02 13:20:24 -0300

    feat: integracao imagens imoveis visualizar todos Desktop

--- src/frontend/src/desktop/components/Imoveis.tsx
+++ src/frontend/src/desktop/components/Imoveis.tsx
@@ -28,22 +28,22 @@ export default function Imoveis() {
 
   const fetchProperties = async () => {
     try {
-      const propertiesResponse = await axiosInstance.get(
-        "property/Imoveis/PegarTodosImoveis"
-      );
-      const usersResponse = await axiosInstance.get(
-        "auth/User/PegarTodosUsuarios"
-      );
+        const propertiesResponse = await axiosInstance.get(
+          "property/Imoveis/PegarTodosImoveis"
+        );
+        const usersResponse = await axiosInstance.get(
+          "auth/User/PegarTodosUsuarios"
+        );
   
       if (!propertiesResponse.data || !usersResponse.data) {
         console.error("Dados de resposta inválidos");
         return;
       }
-  
       const properties = propertiesResponse.data;
       const users = usersResponse.data;
-  
-      // Mesclando os dados de imóveis com locador e locatário
+
+      console.log(users);
+
       const mergedProperties = properties.map(
         (property: {
           imovelId: any;
@@ -59,12 +59,12 @@ export default function Imoveis() {
           complemento: any;
         }) => {
           // Encontrando os dados do locador
-          const landlord =
-            users.find((user: { usuarioId: any }) => user.usuarioId === property.locadorId)?.nome || "Locador não encontrado";
+            const landlord =
+            users.find((user: { roleId: any }) => user.roleId === property.locadorId)?.nome || "Locador não encontrado";
   
           // Encontrando os dados do locatário
           const tenant =
-            users.find((user: { usuarioId: any }) => user.usuarioId === property.locatarioId)?.nome || "Locatário não encontrado";
+            users.find((user: { roleId: any  }) => user.roleId === property.locatarioId)?.nome || "Locatário não encontrado";
   
           return {
             id: property.imovelId,
@@ -74,25 +74,68 @@ export default function Imoveis() {
             propertyType: property.tipoImovel,
             landlord: landlord,
             tenant: tenant,
-            imageSrc: property.fotos?.[0]?.url || "/imovel.png", // Suporte para foto do backend ou imagem padrão
-            price: `R$ ${property.valorImovel.toLocaleString("pt-BR")} | R$ ${
-              property.condominio ? property.condominio.toLocaleString("pt-BR") : 0
-            }`,
+            imageSrc: property.fotos,
+            price: `R$ ${property.valorImovel.toLocaleString("pt-BR")}`,
+            condominio: property.condominio != 0 ? `R$ ${property.condominio}` : "Este imóvel não tem condominio"
           };
         }
       );
-  
-      // Atualizando os estados com os dados mesclados
+
+      // Arrumar imagens para serem exibidas
+      // separar string por vírgula
+      mergedProperties.forEach((property: any) => {
+        if (typeof property.imageSrc === 'string') {
+          property.imageSrc = property.imageSrc.split(";").map((foto: string) => foto.trim());
+        }
+      });
+
+      // array apenas com os nomes dos objetos no Storage
+      const allPhotos = mergedProperties.flatMap((property: any) => {
+        if (!property.imageSrc || property.imageSrc.length === 0) {
+          return []; // sem fotos
+        }
+        // Tirar o prefixo da URL
+        return Array.isArray(property.imageSrc) ? property.imageSrc.map((foto: string) =>
+          foto.replace("https://storage.googleapis.com/administradora-kk.appspot.com/", "")
+        ) : [];
+      });
+
+      // Assinar as URLs das imagens
+      const responsePhotos = await axiosInstance.post('property/Imoveis/AssinarFotos', allPhotos);
+      if (!responsePhotos.data) {
+        console.error("Dados de resposta inválidos do endpoint de assinatura");
+        return;
+      }
+      
+      const signedPhotos: string[] = responsePhotos.data;
+      
+      // Redistribuir as fotos assinadas para cada imóvel usando um offset
+      let offset = 0;
+      mergedProperties.forEach((property: any) => {
+        if (!property.imageSrc) {
+          return; // sem fotos
+        }
+      
+        const count = property.imageSrc.length;
+        // As fotos assinadas desse imóvel estão em [offset, offset + count)
+        const signedSlice = signedPhotos.slice(offset, offset + count);
+      
+        // Atualiza as fotos do imóvel com as URLs assinadas
+        property.imageSrc = signedSlice;
+      
+        // Avança o offset
+        offset += count;
+      });
+
       setProperties(mergedProperties);
       setLoading(false); // Caso a requisição dos dados tenha sido bem sucedida
       setFilteredData(mergedProperties);
-  
-    } catch (error) {
+      console.log(mergedProperties);
+    } catch (error: any) {
       console.error(error);
       showErrorToast("Erro ao se conectar com o servidor.");
     }
   };
-
 
   useEffect(() => {
     const fetchData = async () => {
@@ -166,7 +209,7 @@ export default function Imoveis() {
                   propertyType={property.propertyType}
                   landlord={property.landlord}
                   tenant={property.tenant}
-                  imageSrc={property.imageSrc}
+                  imageSrc={property.imageSrc && property.imageSrc.length > 0 ? property.imageSrc[0] : "../../../public/ImovelSemFoto.png"}
                   price={property.price}
                   condominio={property.condominio} />
               );
