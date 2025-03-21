e6c349ae19aa3fd929078f158a3c29342046b6ee
Matheus R. dos Santos <matheus.ribeiro@sou.inteli.edu.br>
2025-03-21 18:09:07 -0300

    fix: duplicate add payments function

--- src/frontend/src/components/Botoes/Botao.tsx
+++ src/frontend/src/components/Botoes/Botao.tsx
@@ -2,14 +2,16 @@ import React from "react";
 
 interface BotaoProps {
   label: string;
-  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  type: string;
 }
 
-const Botao: React.FC<BotaoProps> = ({ label, onClick }) => {
+const Botao: React.FC<BotaoProps> = ({ label, onClick, type }) => {
   return (
     <div className="flex justify-center w-full">
     <button
       onClick={onClick}
+      type="submit"
       className="w-full h-12 max-w-2xl bg-neutral-900 hover:bg-neutral-black hover:shadow-xl text-neutral-100 py-3 px-4 rounded-lg shadow-sm z-10 transition duration-300"
     >
       {label}
