f1e1152b5ad53e5d432d04f2d3714da1fddbcce6
Matheus R. dos Santos <matheus.ribeiro@sou.inteli.edu.br>
2025-03-21 18:17:57 -0300

    hotfix: type errors with typescript

--- src/frontend/src/components/Botoes/Botao.tsx
+++ src/frontend/src/components/Botoes/Botao.tsx
@@ -3,10 +3,10 @@ import React from "react";
 interface BotaoProps {
   label: string;
   onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
-  type: string;
+  type?: string;
 }
 
-const Botao: React.FC<BotaoProps> = ({ label, onClick, type }) => {
+const Botao: React.FC<BotaoProps> = ({ label, onClick }) => {
   return (
     <div className="flex justify-center w-full">
     <button
