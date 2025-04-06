1e177fd3fdc79ad552570b75c1224710f04287ae
Matheus R. dos Santos <matheus.ribeiro@sou.inteli.edu.br>
2025-04-06 17:16:11 -0300

    fix: navigation buttons dinamically

--- src/frontend/src/desktop/pages/VisualizarChamados.tsx
+++ src/frontend/src/desktop/pages/VisualizarChamados.tsx
@@ -1,75 +1,21 @@
-import { useNavigate } from "react-router-dom";
-import {
-  FaHome,
-  FaClipboardList,
-  FaUsers,
-  FaFileContract,
-} from "react-icons/fa"; // Importing icons
-// import Dashboard from '../components/Dashboard';
 import Chamados from "../components/Chamados";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/FooterSmall";
-import { FaMoneyBillTrendUp } from "react-icons/fa6";
+import NavigationButtons, { UserRole } from "../../components/Navigation/NavigationButtons";
+import { userRoleAtom } from "../../store/atoms";
+import { useAtomValue } from "jotai";
 
 export default function VisualizarChamados() {
-  const navigate = useNavigate();
+  const userRole = useAtomValue(userRoleAtom);
 
   return (
     <div className="flex flex-col min-h-screen bg-[#F0F0F0]">
       <Navbar />
-      {/* <div className='mt-10 mb-24'>
-        <Dashboard />
-      </div> */}
-      {/* New Row with Clickable Options */}
-      <div className="flex justify-center gap-6 ms-2">
-        <button
-          className="relative group flex items-center gap-2 px-4 py-2 text-neutral-800 rounded-md overflow-hidden"
-          onClick={() => navigate("/imoveis")}
-        >
-          <span className="absolute inset-0 -m-2 bg-neutral-400 z-0 scale-0 group-hover:scale-100 transition-transform"></span>
-          <FaHome className="z-10" />
-          <span className="z-10">Imóveis</span>
-        </button>
-
-        <button
-          className="relative group flex items-center gap-2 px-4 py-2 text-neutral-800 rounded-md overflow-hidden"
-          onClick={() => navigate("/usuarios")}
-        >
-          <span className="absolute inset-0 -m-2 bg-neutral-400 z-0 scale-0 group-hover:scale-100 transition-transform"></span>
-          <FaUsers className="z-10" />
-          <span className="z-10">Clientes</span>
-        </button>
-
-        <button
-          className="relative group flex items-center gap-2 px-4 py-2 text-neutral-800 rounded-md overflow-hidden"
-          onClick={() => navigate("/contratos")}
-        >
-          <span className="absolute inset-0 -m-2 bg-neutral-400 z-0 scale-0 group-hover:scale-100 transition-transform"></span>
-          <FaFileContract className="z-10" />
-          <span className="z-10">Contratos</span>
-        </button>
-        <button
-          className="relative group flex items-center gap-2 px-4 py-2 text-neutral-800 rounded-md overflow-hidden"
-          onClick={() => navigate("/pagamentos")}
-        >
-          <span className="absolute inset-0 -m-2 bg-neutral-400 z-0 scale-0 group-hover:scale-100 transition-transform"></span>
-          <FaMoneyBillTrendUp className="z-10" />
-          <span className="z-10">Pagamentos</span>
-        </button>
-        <button
-          className="relative group flex items-center gap-2 px-4 py-2 text-neutral-800 rounded-md overflow-hidden"
-          onClick={() => navigate("/chamados")}
-        >
-          <span className="absolute inset-0 -m-2 bg-neutral-400 z-0 scale-0 group-hover:scale-100 transition-transform"></span>
-          <FaClipboardList className="z-10" />
-          <span className="z-10 font-bold">Chamados</span>
-        </button>
-      </div>
+      <NavigationButtons userRole={userRole as UserRole} />
       <div className="h-[1px] bg-neutral-400 mb-4"></div>
       <div className="flex-grow w-full flex justify-center">
         <Chamados />
       </div>
-
       <Footer />
     </div>
   );
